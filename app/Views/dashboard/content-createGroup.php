b912b0d9ef66b5f4a82ef35efedd6bb319ec4312
ikramzaidan <65273548+ikramzaidan@users.noreply.github.com>
2021-07-17 19:06:37 +0700

    Update View

---
+++ app/Views/dashboard/content-createGroup.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+<body>
+    <form action="group/create" method="post">
+        <div>
+            <input type="text" name="name" placeholder="Group Name">
+        </div>
+        <div>
+            <input type="text" name="desc" placeholder="Group Description">
+        </div>
+        <div>
+            <button type="submit">Buat Grup</button>
+        </div>
+    </form>
+
+</body>
+</html>
--- app/Views/dashboard/sidebar.php
+++ app/Views/dashboard/sidebar.php
@@ -247,9 +247,9 @@
                     <!-- Content -->
                     <?php
                         $db = \Config\Database::connect();
-                        $user = $db->table('users')->getWhere(['username'=>$usrn])->getRowArray();
-                        $group = $db->table('groups')->getWhere(['id_user'=>$user['id']])->getResult();
-                        if(count($group) == 0)
+                        $user   = $db->table('users')->getWhere(['username'=>$usrn])->getRowArray();
+                        $groups  = $db->table('users_groups')->getWhere(['id_user'=>$user['id']])->getResult();
+                        if(count($groups) == 0)
                         {                                      
                     ?>
                     <div class="d-flex flex-column py-5 mt-5">
@@ -260,12 +260,44 @@
                                 <i style="font-size:3rem;" class="fas fa-plus text-gray-800"></i>
                             </a>
                             <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                                <a href="" class="dropdown-item" type="button">Buat Kelas</a>
+                                <a href="<?php base_url() ?>/group" class="dropdown-item" type="button">Buat Kelas</a>
                                 <a href="" class="dropdown-item" type="button">Gabung Kelas</a>
                             </div>
                         </div>
                     </div>
-                    <?php } ?>
+                    <?php 
+                        }else{
+                    ?>
+                    <div class="row">
+                    <?php
+                            foreach($groups as $item)
+                            {
+                                $group  = $db->table('groups')->getWhere(['id'=>$item->id_group])->getRow();
+                    ?>
+                        <!-- Earnings (Monthly) Card Example -->
+                        <div class="col-md-4 mb-4">
+                            <div class="card border-dark h-100">
+                                <div class="card-header pt-3 pb-5 bg-primary">
+                                    <div class="h5 mb-0 font-weight-bold text-light text-uppercase">
+                                        <?= $group->group_name;?>   
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="mb-0 font-weight-bold text-gray-800"><?= $group->group_desc;?></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                            }
+                    ?>
+                    </div>
+                    <?php
+                        }
+                    ?>
                 </div>
                 <!-- /.container-fluid -->
 
