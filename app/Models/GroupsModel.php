6fc0e8fd5d6c53ba2e86e409805474f3a33148ed
ikramzaidan <65273548+ikramzaidan@users.noreply.github.com>
2021-07-17 19:06:01 +0700

    Create Groups Model

---
+++ app/Models/GroupsModel.php
@@ -0,0 +1,22 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class GroupsModel extends Model
+{
+    protected $table = 'groups';
+    protected $allowedFields = ['id','id_user','group_name','group_desc','group_code','group_pass'];
+
+    function saveGroup($data)
+    {
+        $builder = $this->db->table($this->table);
+        $builder->insert($data);
+        $data2 = [
+            'id_user'  => $data['id_user'],
+            'id_group' => $this->db->insertID()
+        ];
+        return $this->db->table('users_groups')->insert($data2);
+    }
+}
+
+?>
