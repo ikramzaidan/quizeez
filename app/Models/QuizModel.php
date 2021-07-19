cca1e8f0496c0db8fb6ccae16521021ceedc4805
ikramzaidan <65273548+ikramzaidan@users.noreply.github.com>
2021-07-19 19:38:51 +0700

    Update QuizModel

--- app/Models/QuizModel.php
+++ app/Models/QuizModel.php
@@ -5,11 +5,12 @@ use CodeIgniter\Model;
 class QuizModel extends Model
 {
     protected $table = 'quiz';
-    protected $allowedFields = ['id','id_user','id_group','name','description','duration'];
+    protected $allowedFields = ['id','id_user','id_group','hash','name','description','duration'];
 
     function createQuiz($data)
     {
-        $builder = $this->db->table($this->table);
+        $builder      = $this->db->table($this->table);
+        $data['hash'] = crypt($this->db->insertID().$data['id_user'].$data['id_group'], md5($data['id_group']));
         return  $builder->insert($data);
     }
 
