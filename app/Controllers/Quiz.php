7dadeb62f1caba291dce8a77c2566c6570b79b10
ikramzaidan <65273548+ikramzaidan@users.noreply.github.com>
2021-07-22 21:31:34 +0700

    Update Quiz Controller
    This update allows the group admin to edit the quiz including the way to create a new question.

--- app/Controllers/Quiz.php
+++ app/Controllers/Quiz.php
@@ -2,7 +2,7 @@
 
 class Quiz extends BaseController
 {
-    public function index($group_hash,$quiz_hash)
+    public function index($group_hash, $quiz_hash)
 	{
         $data          = $this->data;
 		$user          = $this->UserModel->where('username', $this->Session->sess_user)->first();
@@ -17,6 +17,54 @@ class Quiz extends BaseController
 				$data['quiz_hash'] = $quiz_hash;
 				echo view('dashboard/sidebar', $data);
 				echo view('dashboard/content-quiz', $data);
+				echo view('dashboard/footer', $data);
+			}else{
+				return redirect()->to(base_url());
+			}
+		}else{
+			return redirect()->to('/');
+		}
+	}
+
+	public function edit($group_hash, $quiz_hash)
+	{
+        $data          = $this->data;
+		$user          = $this->UserModel->where('username', $this->Session->sess_user)->first();
+		$Group         = $this->db->table('groups')->getWhere(['group_code'=>$group_hash])->getRowArray();
+        $data['usrn']  = $user['username'];
+		$data['group'] = $Group['id'];
+		if($Group){
+			$VerifUser   = $this->db->table('users_groups')->where('id_group', $Group['id'])->getWhere(['id_user'=>$user['id']])->getRowArray();
+			$VerifQuiz   = $this->db->table('quiz')->where('hash', $quiz_hash)->getWhere(['id_group'=>$Group['id']])->getRowArray();
+			if($VerifUser and $VerifQuiz){
+
+				$data['quiz_hash'] = $quiz_hash;
+				echo view('dashboard/sidebar', $data);
+				echo view('dashboard/content-quiz-edit', $data);
+				echo view('dashboard/footer', $data);
+			}else{
+				return redirect()->to(base_url());
+			}
+		}else{
+			return redirect()->to('/');
+		}
+	}
+
+	public function attempt($group_hash, $quiz_hash)
+	{
+        $data          = $this->data;
+		$user          = $this->UserModel->where('username', $this->Session->sess_user)->first();
+		$Group         = $this->db->table('groups')->getWhere(['group_code'=>$group_hash])->getRowArray();
+        $data['usrn']  = $user['username'];
+		$data['group'] = $Group['id'];
+		if($Group){
+			$VerifUser   = $this->db->table('users_groups')->where('id_group', $Group['id'])->getWhere(['id_user'=>$user['id']])->getRowArray();
+			$VerifQuiz   = $this->db->table('quiz')->where('hash', $quiz_hash)->getWhere(['id_group'=>$Group['id']])->getRowArray();
+			if($VerifUser and $VerifQuiz){
+
+				$data['quiz_hash'] = $quiz_hash;
+				echo view('dashboard/sidebar', $data);
+				echo view('dashboard/content-quiz-attempt', $data);
 				echo view('dashboard/footer', $data);
 			}else{
 				return redirect()->to(base_url());
