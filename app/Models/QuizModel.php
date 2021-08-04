6e9b8d70bd2d4a333f61081dd78645d7122a0715
ikramzaidan <65273548+ikramzaidan@users.noreply.github.com>
2021-08-04 19:06:48 +0700

    Update QuizModel

--- app/Models/QuizModel.php
+++ app/Models/QuizModel.php
@@ -5,20 +5,15 @@ use CodeIgniter\Model;
 class QuizModel extends Model
 {
     protected $table = 'quiz';
-    protected $allowedFields = ['id','id_user','id_group','hash','name','description','duration'];
+    protected $allowedFields = ['id','id_user','id_group','hash','name','description','duration','time_open','time_close'];
 
     function createQuiz($data)
     {
         $builder      = $this->db->table($this->table);
-        $data['hash'] = crypt($this->db->insertID().$data['id_user'].$data['id_group'], md5($data['id_group']));
+        $data['hash'] = crypt($this->db->insertID().$data['id_user'].$data['id_group'].$this->db->insertID(), md5($data['name']));
         return  $builder->insert($data);
     }
 
-    function joinGroup($data)
-    {
-        $builder = $this->db->table('users_groups');
-        return $this->db->table('users_groups')->insert($data);
-    }
 }
 
 ?>
